334aca1bafa2f60e2f9a17c9fe0f280d5af9a2ef
Yuto Tanaka <karaage_nennen@icloud.com>
2023-04-18 17:45:07 +0900

    Update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,9 @@
--- NOTE: 関数を保護されたモードで呼び出すpcallを使って、weztermを呼び出す。
--- pcallの最初の戻り値がfalseの時は、funcの実行中にエラーが発生している。
---
--- http://www.rtpro.yamaha.co.jp/RT/docs/lua/tutorial/library.html#pcall
-local status, wezterm = pcall(require, "wezterm")
-if not status then
-  return
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
 end
 
 -- os.dateによって返却された数値から曜日を判定し、漢字に変換する
@@ -27,45 +26,18 @@ local function day_of_week_ja(w_num)
   end
 end
 
--- 年月日と時間、バッテリーの残量をステータスバーに表示する
+-- 年月日と時間をステータスバーに表示する
 -- ウィンドウが最初に表示されてから1秒後に開始され、1秒に1回トリガーされるイベント
 wezterm.on("update-status", function(window, pane)
   -- 日付のtableを作成する方法じゃないと曜日の取得がうまくいかなかった
   -- NOTE: https://www.lua.org/pil/22.1.html
   local wday = os.date("*t").wday
   -- 指定子の後に半角スペースをつけないと正常に表示されなかった
-  local wday_ja = string.format("(%s )", day_of_week_ja(wday))
-  local date = wezterm.strftime("📆  %Y-%m-%d " .. wday_ja .. " ⏰  %H:%M:%S")
-
-  local bat = ""
-
-  for _, b in ipairs(wezterm.battery_info()) do
-    local battery_state_of_charge = b.state_of_charge * 100
-    local battery_icon = ""
-
-    if battery_state_of_charge >= 80 then
-      battery_icon = "🌕  "
-    elseif battery_state_of_charge >= 70 then
-      battery_icon = "🌖  "
-    elseif battery_state_of_charge >= 60 then
-      battery_icon = "🌖  "
-    elseif battery_state_of_charge >= 50 then
-      battery_icon = "🌗  "
-    elseif battery_state_of_charge >= 40 then
-      battery_icon = "🌗  "
-    elseif battery_state_of_charge >= 30 then
-      battery_icon = "🌘  "
-    elseif battery_state_of_charge >= 20 then
-      battery_icon = "🌘  "
-    else
-      battery_icon = "🌑  "
-    end
-
-    bat = string.format("%s%.0f%% ", battery_icon, battery_state_of_charge)
-  end
+  local wday_ja = string.format("(%s)", day_of_week_ja(wday))
+  local date = wezterm.strftime("📆 %Y-%m-%d " .. wday_ja .. " ⏰ %H:%M:%S")
 
   window:set_right_status(wezterm.format({
-    { Text = date .. "  " .. bat },
+    { Text = date .. "  " },
   }))
 end)
 
@@ -81,12 +53,13 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
   return string.format(" %d ", tab_index)
 end)
 
+-- 色の設定
 local base_colors = {
   dark = "#172331",
   yellow = "#ffe64d",
 }
 
-local colors = {
+config.colors = {
   cursor_bg = base_colors["yellow"],
   split = "#6fc3df",
   -- the foreground color of selected text
@@ -101,80 +74,153 @@ local colors = {
       fg_color = base_colors["dark"],
     },
     -- plus button hidden
-    --[[
     new_tab = {
       bg_color = base_colors["dark"],
       fg_color = base_colors["dark"],
     },
-    ]]
   },
 }
 
--- キーバインドの設定、macOSの場合は以下のようになる
---
--- CTRL →  CMD
--- ALT → OPTION
-
+-- キーバインドの設定
 -- leader keyを CTRL + qにマッピング
-local leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
-local act = wezterm.action
-local keys = {
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
   -- CMD + Tでタブを新規作成
-  { key = "t", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
-  -- CMD + Wでタブを閉じる
-  { key = "w", mods = "LEADER", action = act({ CloseCurrentTab = { confirm = true } }) },
-  -- CTRL + q + numberでタブの切り替え
-  { key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
-  { key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
-  { key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
-  { key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
-  { key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
-  { key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
-  { key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
-  { key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
-  { key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
-  -- PANEを水平方向に開く
-  { key = "-", mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-  -- PANEを縦方向に開く
-  { key = "|", mods = "LEADER", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-  -- hjklでPANEを移動する
-  { key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
-  { key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
-  { key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
-  { key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
-  -- PANEを閉じる
-  { key = "x", mods = "ALT", action = act({ CloseCurrentPane = { confirm = true } }) },
-  -- ALT + hjklでペインの幅を調整する
-  { key = "h", mods = "ALT", action = act({ AdjustPaneSize = { "Left", 5 } }) },
-  { key = "l", mods = "ALT", action = act({ AdjustPaneSize = { "Right", 5 } }) },
-  { key = "k", mods = "ALT", action = act({ AdjustPaneSize = { "Up", 5 } }) },
-  { key = "j", mods = "ALT", action = act({ AdjustPaneSize = { "Down", 5 } }) },
-  -- QuickSelect・・・画面に表示されている文字をクイックにコピペできる機能
-  { key = "Enter", mods = "SHIFT", action = "QuickSelect" },
-}
-
--- デフォルトディレクトリを/Documents/に変更
--- NOTE: ~でホームディレクトリを指定する方法だとうまくいかなかった
---local default_cwd = os.getenv('HOME')..'/Documents/'
-
-return {
-  color_scheme = "GitHub Dark",
-  default_cwd = default_cwd,
-  colors = colors,
-  leader = leader,
-  keys = keys,
-  font = wezterm.font("FiraCode Nerd Font Mono", { weight = 450, stretch = "Normal", style = "Normal" }),
-  font_size = 12,
-  line_height = 1,
-  use_fancy_tab_bar = false,
-  -- アクティブではないペインの彩度を変更しない
-  inactive_pane_hsb = {
-    saturation = 1,
-    brightness = 1,
+  {
+    key = "t",
+    mods = "LEADER",
+    action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
   },
-  use_ime = true,
-  -- Home PC
-  default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~" },
-  -- Work PC
-  --default_prog = { "wsl.exe", "--distribution", "Ubuntu-20.04", "--cd", "~" },
+  -- CMD + Wでタブを閉じる
+  {
+    key = "w",
+    mods = "LEADER",
+    action = wezterm.action({ CloseCurrentTab = { confirm = true } }),
+  },
+  -- CTRL + q + numberでタブの切り替え
+  {
+    key = "1",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 0 }),
+  },
+  {
+    key = "2",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 1 }),
+  },
+  {
+    key = "3",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 2 }),
+  },
+  {
+    key = "4",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 3 }),
+  },
+  {
+    key = "5",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 4 }),
+  },
+  {
+    key = "6",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 5 }),
+  },
+  {
+    key = "7",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 6 }),
+  },
+  {
+    key = "8",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 7 }),
+  },
+  {
+    key = "9",
+    mods = "LEADER",
+    action = wezterm.action({ ActivateTab = 8 }),
+  },
+  -- PANEを水平方向に開く
+  {
+    key = "-",
+    mods = "LEADER",
+    action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+  },
+  -- PANEを縦方向に開く
+  {
+    key = "|",
+    mods = "LEADER",
+    action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+  },
+  -- hjklでPANEを移動する
+  {
+    key = "h",
+    mods = "LEADER",
+    action = wezterm.action({ ActivatePaneDirection = "Left" }),
+  },
+  {
+    key = "l",
+    mods = "LEADER",
+    action = wezterm.action({ ActivatePaneDirection = "Right" }),
+  },
+  {
+    key = "k",
+    mods = "LEADER",
+    action = wezterm.action({ ActivatePaneDirection = "Up" }),
+  },
+  {
+    key = "j",
+    mods = "LEADER",
+    action = wezterm.action({ ActivatePaneDirection = "Down" }),
+  },
+  -- PANEを閉じる
+  { key = "x", mods = "ALT", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+  -- ALT + hjklでペインの幅を調整する
+  {
+    key = "h",
+    mods = "ALT",
+    action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }),
+  },
+  {
+    key = "l",
+    mods = "ALT",
+    action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }),
+  },
+  {
+    key = "k",
+    mods = "ALT",
+    action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }),
+  },
+  {
+    key = "j",
+    mods = "ALT",
+    action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }),
+  },
+  -- QuickSelect・・・画面に表示されている文字をクイックにコピペできる機能
+  {
+    key = "Enter",
+    mods = "SHIFT",
+    action = "QuickSelect",
+  },
 }
+
+config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = 450, stretch = "Normal", style = "Normal" })
+config.font_size = 12
+config.line_height = 1
+config.color_scheme = "GitHub Dark"
+config.use_fancy_tab_bar = false
+config.use_ime = true
+-- アクティブではないペインの彩度を変更しない
+config.inactive_pane_hsb = {
+  saturation = 1,
+  brightness = 1,
+}
+-- Work PC
+--config.default_domain = "WSL:Ubuntu-20.04"
+-- Home PC
+config.default_domain = "WSL:Ubuntu"
+
+return config
