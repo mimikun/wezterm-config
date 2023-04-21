d3582629fa7284d12d9b10a3b32030e434d45ad2
Yuto Tanaka <karaage_nennen@icloud.com>
2023-04-21 21:33:12 +0900

    fix error

--- wezterm.lua
+++ wezterm.lua
@@ -11,7 +11,7 @@ end
 local function debug_log_print()
   wezterm.log_info("Config Dir " .. wezterm.config_dir)
   wezterm.log_info("Config file " .. wezterm.config_file)
-  wezterm.log_info("Default hyperlink rules " .. wezterm.default_hyperlink_rules())
+  --wezterm.log_info("Default hyperlink rules " .. wezterm.default_hyperlink_rules())
   wezterm.log_info("Version " .. wezterm.version)
   wezterm.log_info("Exe dir " .. wezterm.executable_dir)
   wezterm.log_info("Default ssh domains" .. wezterm.default_ssh_domains())
